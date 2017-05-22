75ef03dee414a36c14e6404fa92ec6e99cf87919
Serghei Luchianenco <s@luchianenco.com>
2017-05-22 20:21:53 +0200

    User Document - added new fields

--- src/AppBundle/Document/User.php
+++ src/AppBundle/Document/User.php
@@ -16,21 +16,44 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
  */
 class User extends BaseUser
 {
-
+    /**
+     * @MongoDB\Field(type="string")
+     */
     protected $firstName;
 
+    /**
+     * @MongoDB\Field(type="string")
+     */
     protected $lastName;
 
+    /**
+     * @MongoDB\Field(type="string")
+     */
     protected $gender;
 
+    /**
+     * @MongoDB\Field(type="string")
+     */
     protected $team;
 
+    /**
+     * @MongoDB\Field(type="string")
+     */
     protected $progress;
 
+    /**
+     * @MongoDB\Field(type="string")
+     */
     protected $photo;
 
+    /**
+     * @MongoDB\Field(type="string")
+     */
     protected $vk_id;
 
+    /**
+     * @MongoDB\Field(type="string")
+     */
     protected $fb_id;
 
     public function __construct()
@@ -39,4 +62,131 @@ class User extends BaseUser
         // your own logic
     }
 
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param mixed $gender
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProgress()
+    {
+        return $this->progress;
+    }
+
+    /**
+     * @param mixed $progress
+     */
+    public function setProgress($progress)
+    {
+        $this->progress = $progress;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFbId()
+    {
+        return $this->fb_id;
+    }
+
+    /**
+     * @param mixed $fb_id
+     */
+    public function setFbId($fb_id)
+    {
+        $this->fb_id = $fb_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTeam()
+    {
+        return $this->team;
+    }
+
+    /**
+     * @param mixed $team
+     */
+    public function setTeam($team)
+    {
+        $this->team = $team;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    /**
+     * @param mixed $photo
+     */
+    public function setPhoto($photo)
+    {
+        $this->photo = $photo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVkId()
+    {
+        return $this->vk_id;
+    }
+
+    /**
+     * @param mixed $vk_id
+     */
+    public function setVkId($vk_id)
+    {
+        $this->vk_id = $vk_id;
+    }
 }
