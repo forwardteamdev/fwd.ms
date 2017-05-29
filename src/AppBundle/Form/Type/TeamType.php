0a2601c93f7d1ffd9364a6f4070c3e3e9be5ed95
Serghei Luchianenco <s@luchianenco.com>
2017-05-29 20:30:00 +0200

    Team form type

---
+++ src/AppBundle/Form/Type/TeamType.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author Serghei Luchianenco (s@luchianenco.com)
+ * Date: 29/05/2017
+ * Time: 18:52
+ */
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TeamType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'csrf_protection' => false
+        ]);
+    }
+}
